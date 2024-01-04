e88d208feacd5fee45198ed67666465ce1dcadcd
Sophie <petya.antal@gmail.com>
2024-01-04 13:24:19 +0100

    állat hozzáadása

--- zoo.py
+++ zoo.py
@@ -1,25 +1,32 @@
-zoo = list()
+def placeanimal(zoo, name)->list:
+    animal = {}
+    found = False
+    for a in(zoo):
+        if name in a.keys(): 
+            a[name] += 1
+            found = True
+        
+    if not found:
+            animal[name] =1
+            zoo.append(animal)
+    return zoo
 
+
+zoo = [{"kecske": 1}, {"béka": 1}]
 animal = {}
 print("This is a Zoo. New animal (1)-Delete one(2)-Exit(0)")
 select = None
 while select != 0:
     select = int(input("What do you want to do? "))
-    
     if select != 0:
-        
         if select == 1:
-            
             name = input("Name of the animal: ")
-            if name not in animal.keys():
-                animal[name] = 1
-                zoo.append(animal)
-            else: 
-                for a in zoo:
-                    a[name] += 1
-            animal = dict()
+            placeanimal(zoo, name)
+            
+                #else: 
+                #    animal[name] = 1
+                #    zoo.append(animal)
+                #    animal = {}
 
-for a in zoo:
-    print(a)
- 
+print(zoo)         
 print("Vége a játéknak.")   
